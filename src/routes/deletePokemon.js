bb41de9afc020a5572eee8f1104437c1beaac7a6
David LEROUX <dleroux63160@gmail.com>
2023-02-02 16:55:10 +0100

    add HTTP status with error message

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -3,14 +3,22 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
     app.delete('/api/pokemons/:id', (req, res) => {
         Pokemon.findByPk(req.params.id).then(pokemon => {
-            const pokemonDeleted = pokemon;
-            Pokemon.destroy({
-                    where: { id: pokemon.id }
-                })
-                .then(_ => {
-                    const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`
-                    res.json({ message, data: pokemonDeleted })
-                })
-        })
+                if (pokemon == null) {
+                    const message = " Le pokémon n'existe pas.Réessayez avec un autre identifiant. "
+                    res.statut(404).json({ message })
+                }
+                const pokemonDeleted = pokemon;
+                Pokemon.destroy({
+                        where: { id: pokemon.id }
+                    })
+                    .then(_ => {
+                        const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`
+                        res.json({ message, data: pokemonDeleted })
+                    })
+            })
+            .catch(error => {
+                const message = " Le pokémon n'a pas pu être supprimé. Réessayez dans quelques instants. "
+                res.statut(500).json({ message, data: error })
+            })
     })
 }
