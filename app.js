92e00473c2b2469bf5850e84b7846f3a01390811
David LEROUX <dleroux63160@gmail.com>
2023-02-09 10:05:22 +0100

    production heroku

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
-const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const sequelize = require('./src/db/sequelize')
 const favicon = require('serve-favicon')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
-    .use(morgan('dev'))
     .use(bodyParser.json())
 
 sequelize.initDb()
+
+app.get('/', (req, res) => {
+        res.json('Hello, Heroku !')
+    })
     // Ici , on place nos différents points de terminaison .
 require('./src/routes/findAllPokemons.js')(app)
 require('./src/routes/findPokemonByPk.js')(app)
